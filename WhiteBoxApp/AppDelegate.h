e3d080f696af126a8ec3d5df8305ed59a39d9e04
AntonHome <antonpetrovmak@gmail.com>
2016-06-05 13:59:06 +0300

    Initial Commit

---
+++ WhiteBoxApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhiteBoxApp
+//
+//  Created by Develop on 05.06.16.
+//  Copyright © 2016 antonpetrovmak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhiteBoxApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhiteBoxApp
+//
+//  Created by Develop on 05.06.16.
+//  Copyright © 2016 antonpetrovmak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
